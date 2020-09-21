f8f51a426bb4516fb2c7ce9bbd4968a2b7be92af
Donald_Draper <shaoqinghan@aliyun.com>
2020-09-21 23:10:19 +0800

    注册器，通知监听器

--- dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/NotifyListener.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/NotifyListener.java
@@ -29,14 +29,25 @@ public interface NotifyListener {
 
     /**
      * Triggered when a service change notification is received.
+     * 当接收服务变更通知时，触发
      * <p>
      * Notify needs to support the contract: <br>
-     * 1. Always notifications on the service interface and the dimension of the data type. that is, won't notify part of the same type data belonging to one service. Users do not need to compare the results of the previous notification.<br>
-     * 2. The first notification at a subscription must be a full notification of all types of data of a service.<br>
-     * 3. At the time of change, different types of data are allowed to be notified separately, e.g.: providers, consumers, routers, overrides. It allows only one of these types to be notified, but the data of this type must be full, not incremental.<br>
-     * 4. If a data type is empty, need to notify a empty protocol with category parameter identification of url data.<br>
-     * 5. The order of notifications to be guaranteed by the notifications(That is, the implementation of the registry). Such as: single thread push, queue serialization, and version comparison.<br>
      *
+     * 1. Always notifications on the service interface and the dimension of the data type.
+     * that is, won't notify part of the same type data belonging to one service. Users do not need to compare the results of the previous notification.<br>
+     * 当服务接口和数据类型，将会通知。同一服务同一数据类型，不会通知。用户不需要比较先前的通知结果。
+     * 2. The first notification at a subscription must be a full notification of all types of data of a service.<br>
+     *     订阅的一个通知，必须是服务全数据类型的通知
+     * 3. At the time of change, different types of data are allowed to be notified separately,
+     * e.g.: providers, consumers, routers, overrides. It allows only one of these types to be notified,
+     * but the data of this type must be full, not incremental.<br>
+     *     随着时间个变更，不同的数据类型，允许分开通知
+     *     比如：提供者，消费者，路由，重写。允许这些类型的通知，但数据类型是全量的，不是增量的。
+     * 4. If a data type is empty, need to notify a empty protocol with category parameter identification of url data.<br>
+     *     如果数据类型为空，需要通知一个url数据的分类参数识别协议
+     * 5. The order of notifications to be guaranteed by the notifications(That is, the implementation of the registry).
+     * Such as: single thread push, queue serialization, and version comparison.<br>
+     *  通知的顺序，依赖注册器的实现，比如单线程推送，队列序列化，版本比较等
      * @param urls The list of registered information , is always not empty. The meaning is the same as the return value of {@link org.apache.dubbo.registry.RegistryService#lookup(URL)}.
      */
     void notify(List<URL> urls);
--- dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/RegistryService.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/org/apache/dubbo/registry/RegistryService.java
@@ -30,18 +30,24 @@ public interface RegistryService {
 
     /**
      * Register data, such as : provider service, consumer address, route rule, override rule and other data.
+     * 注册数据，比如：服务提供者，消费地址，路由规则
      * <p>
      * Registering is required to support the contract:<br>
+     *     注册需要遵循如下约定：
      * 1. When the URL sets the check=false parameter. When the registration fails,
      * the exception is not thrown and retried in the background. Otherwise, the exception will be thrown.<br>
+     *     当Url设置check=false参数，当注册失败时，不会抛出异常，并在后端重新尝试，否则异常将会抛出
      * 2. When URL sets the dynamic=false parameter, it needs to be stored persistently,
      * otherwise, it should be deleted automatically when the registrant has an abnormal exit.<br>
+     *     当url设置dynamic=false，则需要持久化存储，否则，当注册者非正常退出是，将会自动删除。
      * 3. When the URL sets category=routers, it means classified storage,
      * the default category is providers, and the data can be notified by the classified section. <br>
+     *     当url设置category=routers时，意味着分类存储，默认为提供者，在分类节点，将会通知数据变更
      * 4. When the registry is restarted, network jitter, data can not be lost,
      * including automatically deleting data from the broken line.<br>
+     *     当注册器重新启动，网络抖动，数据将不会丢失，包括从断点处，自动删除的数据
      * 5. Allow URLs which have the same URL but different parameters to coexist,they can't cover each other.<br>
-     *
+     *    允许相同url不同的参数共存的场景，不会相互覆盖
      * @param url  Registration information , is not allowed to be empty, e.g: dubbo://10.20.153.10/org.apache.dubbo.foo.BarService?version=1.0.0&application=kylin
      */
     void register(URL url);
@@ -50,10 +56,12 @@ public interface RegistryService {
      * Unregister
      * <p>
      * Unregistering is required to support the contract:<br>
+     *     注销，需要遵循以下契约：
      * 1. If it is the persistent stored data of dynamic=false, the registration data can not be found,
      * then the IllegalStateException is thrown, otherwise it is ignored.<br>
+     *     如果为持久化存储，注册数据将不能被发现时，将会排除非法状态异常，否则忽略
      * 2. Unregister according to the full url match.<br>
-     *
+     *     根据全路径匹配注销
      * @param url Registration information , is not allowed to be empty,
      *           e.g: dubbo://10.20.153.10/org.apache.dubbo.foo.BarService?version=1.0.0&application=kylin
      */
@@ -61,18 +69,27 @@ public interface RegistryService {
 
     /**
      * Subscribe to eligible registered data and automatically push when the registered data is changed.
+     * 订阅合格的注册数据，当注册数据变更是，自动推送
      * <p>
      * Subscribing need to support contracts:<br>
+     *     订阅需要支持一下约定
      * 1. When the URL sets the check=false parameter. When the registration fails, the exception is not thrown and retried in the background. <br>
+     *      当Url设置check=false参数，当注销失败时，不会抛出异常，并在后端重新尝试
      * 2. When URL sets category=routers, it only notifies the specified classification data.
      * Multiple classifications are separated by commas, and allows asterisk to match, which indicates that all categorical data are subscribed.<br>
+     *     当url设置category=routers时，将会通知特定分类的数据。多分类将会以逗号隔开，允许星号匹配，这意味着订阅所有分类的数据。
      * 3. Allow interface, group, version, and classifier as a conditional query,
      * e.g.: interface=org.apache.dubbo.foo.BarService&version=1.0.0<br>
+     *     允许接口，分组，版本，分类作为查询条件
      * 4. And the query conditions allow the asterisk to be matched, subscribe to all versions of all the packets of all interfaces,
      * e.g. :interface=*&group=*&version=*&classifier=*<br>
+     *     查询条件允许型号匹配，订阅所有包中的所有版本接口
      * 5. When the registry is restarted and network jitter, it is necessary to automatically restore the subscription request.<br>
+     *     当注册器重启时，需要重新自动存储订阅请求
      * 6. Allow URLs which have the same URL but different parameters to coexist,they can't cover each other.<br>
+     *     允许相同url不同的参数共存的场景，不会相互覆盖
      * 7. The subscription process must be blocked, when the first notice is finished and then returned.<br>
+     *     在一个通知完成，订阅进程必须被阻塞，然后返回
      *
      * @param url      Subscription condition, not allowed to be empty,
      *                e.g. consumer://10.20.153.10/org.apache.dubbo.foo.BarService?version=1.0.0&application=kylin
@@ -84,9 +101,11 @@ public interface RegistryService {
      * Unsubscribe
      * <p>
      * Unsubscribing is required to support the contract:<br>
+     *     取消订阅需要支持一下约定
      * 1. If don't subscribe, ignore it directly.<br>
+     *     如果不需要订阅，则直接忽略
      * 2. Unsubscribe by full URL match.<br>
-     *
+     *    根据全路径匹配取消订阅
      * @param url      Subscription condition, not allowed to be empty,
      *                 e.g. consumer://10.20.153.10/org.apache.dubbo.foo.BarService?version=1.0.0&application=kylin
      * @param listener A listener of the change event, not allowed to be empty
@@ -96,7 +115,7 @@ public interface RegistryService {
     /**
      * Query the registered data that matches the conditions.
      * Corresponding to the push mode of the subscription, this is the pull mode and returns only one result.
-     *
+     * 查询匹配给定条件的注册数据。对应于订阅的推模式，此为拉模式，并返回一个结果
      * @param url Query condition, is not allowed to be empty, e.g.
      *            consumer://10.20.153.10/org.apache.dubbo.foo.BarService?version=1.0.0&application=kylin
      * @return The registered information list, which may be empty,
